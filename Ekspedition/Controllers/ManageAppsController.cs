48d35c54429fd99275451143245796c8c49a4376
Andri Nur Wakhid <andriwakhid01@gmail.com>
2018-09-18 10:16:44 +0700

    v2

---
+++ Ekspedition/Controllers/ManageAppsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ekspedition.Controllers
+{
+    public class ManageAppsController : Controller
+    {
+        private ExpeditionEntities db = new ExpeditionEntities();
+        // GET: ManageApps
+        public async Task<ActionResult> ListCategory()
+        {
+            return View(await db.Categories.ToListAsync());
+        }
+        public async Task<ActionResult> ListStatusShipping()
+        {
+            return View(await db.StatusShippings.ToListAsync());
+        }
+    }
+}
